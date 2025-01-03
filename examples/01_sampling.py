a52b3488b5f67204f4532c82921e278a49923941
vanhoanglepsa <82471338+vanhoanglepsa@users.noreply.github.com>
2025-01-03 15:41:56 +0700

    update

--- examples/01_sampling.py
+++ examples/01_sampling.py
@@ -32,77 +32,77 @@ datasets = [
 
 benchmark = {
     'HDFS': {
-        'log_file': 'HDFS/HDFS_2k.log',
+        'log_file': 'HDFS/HDFS_full.log',
         'log_format': '<Date> <Time> <Pid> <Level> <Component>: <Content>',
     },
 
     'Hadoop': {
-        'log_file': 'Hadoop/Hadoop_2k.log',
+        'log_file': 'Hadoop/Hadoop_full.log',
         'log_format': '<SessionId> <Date> <Time> <Level> \[<Process>\] <Component>: <Content>',
     },
 
     'Spark': {
-        'log_file': 'Spark/Spark_2k.log',
+        'log_file': 'Spark/Spark_full.log',
         'log_format': '<Date> <Time> <Level> <Component>: <Content>',
     },
 
     'Zookeeper': {
-        'log_file': 'Zookeeper/Zookeeper_2k.log',
+        'log_file': 'Zookeeper/Zookeeper_full.log',
         'log_format': '<Date> <Time> - <Level>  \[<Node>:<Component>@<Id>\] - <Content>',
     },
 
     'BGL': {
-        'log_file': 'BGL/BGL_2k.log',
+        'log_file': 'BGL/BGL_full.log',
         'log_format': '<Label> <Timestamp> <Date> <Node> <Time> <NodeRepeat> <Type> <Component> <Level> <Content>',
     },
 
     'HPC': {
-        'log_file': 'HPC/HPC_2k.log',
+        'log_file': 'HPC/HPC_full.log',
         'log_format': '<LogId> <Node> <Component> <State> <Time> <Flag> <Content>',
     },
 
     'Thunderbird': {
-        'log_file': 'Thunderbird/Thunderbird_2k.log',
+        'log_file': 'Thunderbird/Thunderbird_full.log',
         'log_format': '<Label> <Timestamp> <Date> <User> <Month> <Day> <Time> <Location> <Component>(\[<PID>\])?: <Content>',
     },
 
     'Windows': {
-        'log_file': 'Windows/Windows_2k.log',
+        'log_file': 'Windows/Windows_full.log',
         'log_format': '<Date> <Time>, <Level>                  <Component>    <Content>',
     },
 
     'Linux': {
-        'log_file': 'Linux/Linux_2k.log',
+        'log_file': 'Linux/Linux_full.log',
         'log_format': '<Month> <Date> <Time> <Level> <Component>(\[<PID>\])?: <Content>',
     },
 
     'HealthApp': {
-        'log_file': 'HealthApp/HealthApp_2k.log',
+        'log_file': 'HealthApp/HealthApp_full.log',
         'log_format': '<Time>\|<Component>\|<Pid>\|<Content>',
     },
 
     'Apache': {
-        'log_file': 'Apache/Apache_2k.log',
+        'log_file': 'Apache/Apache_full.log',
         'log_format': '\[<Time>\] \[<Level>\] <Content>',
     },
 
     'Proxifier': {
-        'log_file': 'Proxifier/Proxifier_2k.log',
+        'log_file': 'Proxifier/Proxifier_full.log',
         'log_format': '\[<Time>\] <Program> - <Content>',
     },
 
     'OpenSSH': {
-        'log_file': 'OpenSSH/OpenSSH_2k.log',
+        'log_file': 'OpenSSH/OpenSSH_full.log',
         'log_format': '<Date> <Day> <Time> <Component> sshd\[<Pid>\]: <Content>',
     },
 
     'OpenStack': {
-        'log_file': 'OpenStack/OpenStack_2k.log',
+        'log_file': 'OpenStack/OpenStack_full.log',
         'log_format': '<Logrecord> <Date> <Time> <Pid> <Level> <Component> \[<ADDR>\] <Content>',
     },
 
     'Mac': {
-        'log_file': 'Mac/Mac_2k.log',
+        'log_file': 'Mac/Mac_full.log',
         'log_format': '<Month>  <Date> <Time> <User> <Component>\[<PID>\]( \(<Address>\))?: <Content>',
     }
 }
@@ -116,7 +116,7 @@ if __name__ == '__main__':
     for dataset in datasets:
         print(dataset)
         os.makedirs(f'{output_dir}/{dataset}/samples', exist_ok=True)
-        log_file = benchmark[dataset]['log_file'].replace('2k','full')
+        log_file = benchmark[dataset]['log_file']
         print(f"Loading {log_file}...")
         labelled_logs = pd.read_csv(f'{data_dir}/{log_file}_structured.csv')
         print(f"Loaded {len(labelled_logs)} logs.")
